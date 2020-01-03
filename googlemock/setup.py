bd7ec67334fca13343f677e9588600bcfa031f79
Maxime Roussin-Bélanger <maxime.roussin-belanger@sonatest.com>
2020-01-03 12:34:34 -0500

    googlemock: make it packageable into a python module

---
+++ googlemock/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='googlemock',
+    version='1.10.0',
+    description='Generator of C++ Mock Class',
+    url='https://github.com/Sonatest/googletest/scripts/generator',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='googlemock mock',
+    packages=['googlemock'],
+    package_dir={'googlemock': 'googlemock/generator/cpp'},
+)
